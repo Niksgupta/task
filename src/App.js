e1b19e366bee9a6a3a67a7990e1a6c2e0c05bfa6
nikhil <nikhilatoliya12@gmail.com>
2020-11-18 17:02:06 +0530

    commited

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
 // import logo from './logo.svg';
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
+// import Navbar from 'react-bootstrap/Navbar';
+// import Nav from 'react-bootstrap/Nav';
 import './App.css';
-import Card from 'react-bootstrap/Card';
-import CardDeck from 'react-bootstrap/CardDeck';
-import MenuItem from '@material-ui/core/MenuItem';
-import IconButton from '@material-ui/core/IconButton';
-import Badge from '@material-ui/core/Badge';
+// import Card from 'react-bootstrap/Card';
+// import CardDeck from 'react-bootstrap/CardDeck';
+// import IconButton from '@material-ui/core/IconButton';
+// import Badge from '@material-ui/core/Badge';
 
 // import ListGroup from 'react-bootstrap/ListGroup';
 // import ListGroupItem from 'react-bootstrap/ListGroupItem';
-import { Container } from 'react-bootstrap';
+// import { Container } from 'react-bootstrap';
 // import CardColumns from 'react-bootstrap/CardColumns';
-import SportsEsportsIcon from '@material-ui/icons/SportsEsports';
-import SearchIcon from '@material-ui/icons/Search';
-import NotificationsIcon from '@material-ui/icons/Notifications';
-import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
-import Button from '@material-ui/core/Button';
-import ShareIcon from '@material-ui/icons/Share';
+// import SportsEsportsIcon from '@material-ui/icons/SportsEsports';
+// import SearchIcon from '@material-ui/icons/Search';
+// import NotificationsIcon from '@material-ui/icons/Notifications';
+// import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+// import Button from '@material-ui/core/Button';
+// import ShareIcon from '@material-ui/icons/Share';
 
 import Footer from "./components/Footer";
 import Third from "./components/Third";
